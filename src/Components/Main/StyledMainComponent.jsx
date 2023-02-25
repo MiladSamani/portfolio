0ddfedf4b80548263ac76124a75d8ce225e68bc0
“Milad <Eng.Milad.Samani@gmail.com>
2023-02-25 15:28:34 +0330

    add cursor pointer to social media!

--- src/Components/Main/StyledMainComponent.jsx
+++ src/Components/Main/StyledMainComponent.jsx
@@ -32,6 +32,7 @@ export const SocialIcons = styled.div`
 `;
 
 export const SocialMedia = styled.img`
+cursor: pointer;
   padding: 5px 30px;
   border: 1px solid white;
   background-color: rgb(197 200 212/0.15);

b47c8584d5c1502d636342849a12d0708cf661a9
“Milad <Eng.Milad.Samani@gmail.com>
2023-02-26 18:32:41 +0330

    add mock api to project

--- src/Components/Projects/ProjectsComponent.jsx
+++ src/Components/Projects/ProjectsComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   CardContainer,
@@ -23,25 +23,49 @@ import githubImage from "../../Assets/images/github.svg";
 import watchImage from "../../Assets/images/watchImage.svg";
 
 import secondProject from "../../Assets/images/cm.gif";
+import axios from "axios";
 
 function ProjectsComponent() {
+  const [data, setData] = useState([
+    {
+      image: "https://loremflickr.com/640/480/technics",
+      title: "title 1",
+      details: "details 1",
+      tech1: "tech1 1",
+      tech2: "tech2 1",
+      tech3: "tech3 1",
+      id: "1",
+    },
+    {
+      image: "https://loremflickr.com/640/480/technics",
+      title: "title 2",
+      details: "details 2",
+      tech1: "tech1 2",
+      tech2: "tech2 2",
+      tech3: "tech3 2",
+      id: "2",
+    },
+  ]);
+
+
+
   return (
     <Container>
       <Header src={projectsImage} />
       <CardContainer>
-        <Card>
+        
+{data.map((d)=>(
+  <Card>
           <CardImage src={secondProject} />
           <CardDetails>
-            <CardTitle>Project Name</CardTitle>
+            <CardTitle>{d.title}</CardTitle>
             <CardLine />
-            <CardDescription>
-              Project Details and etc
-            </CardDescription>
+            <CardDescription>{d.details}</CardDescription>
           </CardDetails>
           <CardTechnologies>
-            Technologies: / <span className="blue"> CoreUi </span> /
-            <span className="red"> React.js </span> /
-            <span className="orange"> Javascript </span>
+            Technologies: / <span className="blue"> {d.tech1}</span> /
+            <span className="red"> {d.tech2} </span> /
+            <span className="orange"> {d.tech3} </span>
           </CardTechnologies>
           <CardButton>
             <LeftButton>
@@ -52,30 +76,7 @@ function ProjectsComponent() {
             </RightButton>
           </CardButton>
         </Card>
-
-        <Card>
-          <CardImage src={secondProject} />
-          <CardDetails>
-            <CardTitle>Project Name</CardTitle>
-            <CardLine />
-            <CardDescription>
-            Project Details and etc
-            </CardDescription>
-          </CardDetails>
-          <CardTechnologies>
-            Technologies: / <span className="blue"> CoreUi </span> /
-            <span className="red"> React.js </span> /
-            <span className="orange"> Javascript </span>
-          </CardTechnologies>
-          <CardButton>
-            <LeftButton>
-              <ImageBtn src={githubImage} alt="" />
-            </LeftButton>
-            <RightButton>
-              <ImageBtn src={watchImage} alt="" />
-            </RightButton>
-          </CardButton>
-        </Card>
+))}
       </CardContainer>
       {/* for future and more projects and designs */}
       {/* <DesignButton>More</DesignButton> */}
